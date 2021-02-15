46f1399c61eb96f099224253af8693f01bc01e18
Mustafa Uysal <mustafauyysl@gmail.com>
2021-02-14 16:24:51 -0800

    restock-ui

--- FacebookHomeUI/StoriesView.swift
+++ FacebookHomeUI/StoriesView.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct StoriesView: View {
+    let stories: [String]
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 3) {
+                ForEach(stories, id: \.self) { name in
+                    Image(name)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 140, height: 200, alignment: .center)
+                        .background(Color.red)
+                        .clipped()
+                }
+            }
+            .padding()
+        }
+    }
+}
